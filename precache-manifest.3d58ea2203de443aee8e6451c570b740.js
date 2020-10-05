65ce0ca49c56ef74467a6cd17bbfa03cabd82f7d
AntonPanda08 <roll2001@ukr.net>
2020-10-05 12:51:06 +0300

    Updates

--- precache-manifest.3d58ea2203de443aee8e6451c570b740.js
+++ precache-manifest.3d58ea2203de443aee8e6451c570b740.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "33ffe6e14dd71f6fd6c2cefa8002cc27",
+    "revision": "72ca2163f9a47d87c1c0c1b0d96c668d",
     "url": "/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "97b307748d9e2a65770b",
+    "revision": "8b28441e2e2d61046626",
     "url": "/goit-react-hw-01-components/static/css/main.a1673929.chunk.css"
   },
   {
-    "revision": "8d9dc58c1a4c85358da3",
-    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+    "revision": "2ddef128823e12e989bc",
+    "url": "/goit-react-hw-01-components/static/js/2.d4240624.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+    "url": "/goit-react-hw-01-components/static/js/2.d4240624.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "97b307748d9e2a65770b",
-    "url": "/goit-react-hw-01-components/static/js/main.0bfaa903.chunk.js"
+    "revision": "8b28441e2e2d61046626",
+    "url": "/goit-react-hw-01-components/static/js/main.ed52f9a4.chunk.js"
   },
   {
     "revision": "7c1abe1df921e466788f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.77edb2152d2294f7fc8f348ff5a85abd.js"
+  "/goit-react-hw-01-components/precache-manifest.3d58ea2203de443aee8e6451c570b740.js"
 );
 
 self.addEventListener('message', (event) => {
